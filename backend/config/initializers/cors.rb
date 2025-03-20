b5277ec393e0062b95bd0790421ca3198062dab3
Emmanuel Monehin <e.monehin@live.com>
2025-03-20 13:17:49 +0100

    feat: update CORS configuration to allow specific origins and modify frontend start script to set custom port

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -5,12 +5,13 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "monehin.com", "task-manager.monehin.com", "https://task-manager-n85p-git-main-monehins-projects.vercel.app/"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      credentials: false
+  end
+end
